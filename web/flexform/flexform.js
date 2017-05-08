2544cd72fe090784031e2da3d4ede60dc46ab0d3
ImonCloud <dev@imoncloud.com>
2017-05-08 17:37:20 +0800

    syhu/scalra-flexform: add: /views/list_filter.ejs to list data with optional field filters

--- web/flexform/flexform.js
+++ web/flexform/flexform.js
@@ -61,6 +61,16 @@ $('.contactUs').on('click', function(e){
 //$('.contactUs').click();
 });
 
+// ref: http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
 function uploadFile(account, onDone) {
 	var upload_url = (window.location.protocol + '//' + window.location.hostname + ':' + basePort);
