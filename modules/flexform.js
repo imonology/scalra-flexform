64a0717109c2e90f0d3334df296f708710d29dd2
ImonCloud <dev@imoncloud.com>
2018-05-08 10:04:04 +0800

    kevinho627627/scalra-flexform: *fix english bug

--- modules/flexform.js
+++ modules/flexform.js
@@ -77,7 +77,7 @@ l_module.start = function (config, onDone) {
 					form_name = 'FlexForm:' + l_form[form_id].name;
 					l_form_values[form_id] = ref[form_name];
 
-					LOG.warn( l_form_values[form_id]);
+					// LOG.warn( l_form_values[form_id]);
 
 					l_form[form_id].data.values = {};
 					l_form[form_id].add = l_form_values[form_id].add;
@@ -92,7 +92,7 @@ l_module.start = function (config, onDone) {
 					}
 
 					LOG.warn('form: ' + l_form[form_id].name );
-					LOG.warn(l_form[form_id].data.values);
+					// LOG.warn(l_form[form_id].data.values);
 
 					// l_form[form_id].data.values = ref[form_name];
 
@@ -442,7 +442,7 @@ SR.API.add('QUERY_GROUP_BY_PARTIAL',{
 		}
 		
 		LOG.warn('查詢部分符合之群組');
-		LOG.warn(form);
+		// LOG.warn(form);
 		onDone(null, form.data.values);
 	});
 });
@@ -492,7 +492,7 @@ SR.API.add('CREATE_FORM', {
 			}
 			LOG.warn('INIT FORM成功');
 			LOG.warn('l_form[form.id]');
-			LOG.warn(l_form[form.id]);
+			// LOG.warn(l_form[form.id]);
 			
 			
 			l_form[form.id].data.values = {};
@@ -606,10 +606,10 @@ SR.API.add('QUERY_FORM', {
 			
 			for (var key in args.select_time) {
 				if (fields[key] && fields[key].type === 'date') {
-					LOG.warn('key: ' + key);
-					LOG.warn('compare record: ' + record[key] + ' with query: ' + args.select_time[key]); 
+					// LOG.warn('key: ' + key);
+					// LOG.warn('compare record: ' + record[key] + ' with query: ' + args.select_time[key]); 
 					if (args.select_time[key].start && args.select_time[key].end) {
-						LOG.warn('compare '+ record[key]+ ' start: ' + args.select_time[key].start + ' end: ' + args.select_time[key].end );
+						// LOG.warn('compare '+ record[key]+ ' start: ' + args.select_time[key].start + ' end: ' + args.select_time[key].end );
 						if (record[key] < args.select_time[key].start || record[key] > args.select_time[key].end) {
 							matched = false;
 							break;
@@ -1085,8 +1085,8 @@ var l_add_form = function (para, onDone) {
 			return onDone('values not found for record id [' + para.para.record_id + ']');
 		
 		for (key in para.values_map){
-			LOG.warn('存的key ' + key);
-			LOG.warn(para.form.data.values[para.para.record_id][key] + ' 設成 ' + para.values_map[key]);
+			// LOG.warn('存的key ' + key);
+			// LOG.warn(para.form.data.values[para.para.record_id][key] + ' 設成 ' + para.values_map[key]);
 			l_form_values[para.form.id][para.para.record_id].values[key] = para.values_map[key];
 		}
 
@@ -1737,7 +1737,7 @@ function extract_excel_fields(arr_data, para, onDone, warn_empty, key_field) {
 		// check if we've found all fields
 		if (Object.keys(field_index).length === import_fields.length) {
 			LOG.warn('field row found at: ' + i, l_name);
-			LOG.warn(arr_data[i], l_name);
+			// LOG.warn(arr_data[i], l_name);
 			break;
 		}
 	}
