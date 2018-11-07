ff700b0693f49ba7fc4f3c56494acc06a9348c32
sunnyworm <sunnyworm@gmail.com>
2018-11-07 09:56:51 +0800

    設定為 invisible 時增加 display: none; 屬性，並將下拉式選單的第一個選項設定為空字串 (default option)

--- web/flexform/flexform_v3.js
+++ web/flexform/flexform_v3.js
@@ -1,8 +1,9 @@
 var upload_record_id = [];
 var create_table_v3 = function (form, para) {
 	var hide = para.hide;
+	var invisible = para.invisible;
 	// console.log('hide = ');
-	// console.log(hide)
+	// console.log(invisible);
 	var write = para.write;
 	var td_style = para.td_style;
 	var show = para.show;
@@ -14,12 +15,15 @@ var create_table_v3 = function (form, para) {
 	else
 		var id_num = para.id_num;
 	var hide = (hide ? hide : []);
-	if (!td_style)
+
+	if (!td_style) {
 		td_style =  ['width:15%;','text-align:left;'];
+	}
+
 	var html = '';
-	
+
 	var fields = form.data.fields;
-	
+
 	function check_lock(id, lock) {
 		for(var i in lock){
 			if (lock[i] === id)
@@ -53,9 +57,9 @@ var create_table_v3 = function (form, para) {
 				// console.log('有default_value' + fields[i].default_value)
 			// check if the field is hidden or specified as 'hide'
 			if (!fields[i].show || hide.indexOf(fields[i].id) !== (-1)) {
-				continue;				
+				continue;
 			}
-			
+
 			if (customized) {
 				var col_f = fields[i].col_f;
 				var col_v = fields[i].col_v;
@@ -65,7 +69,7 @@ var create_table_v3 = function (form, para) {
 				var col_v = 1;
 				var br = true;
 			}
-			
+
 			// console.log(fields[i].name + ' col_f = ' + col_f + ' col_v = ' + col_v + ' br = ' + br);
 			// begin a row of data
 			if (show) {
@@ -76,15 +80,15 @@ var create_table_v3 = function (form, para) {
 			}
 			// else
 				// html += '<tr>';
-			
+
 			if (fields[i].type === 'line') {
 				html += '<tr><td colspan="'+(col_f + col_v)+'"><hr></td>';
 			} else {
 				// display field name
 
-				html += '<td  '+(fields[i].type === 'print'? 'class="print"' : '')+' style="'+(td_style?td_style[0]:'')+' ; vertical-align:middle;" colspan="'+col_f+'">' + fields[i].name + (fields[i].must ? '' : '') +  ' </td>';
+				html += '<td  '+(fields[i].type === 'print'? 'class="print"' : '')+' style="'+(td_style?td_style[0]:'')+' ; vertical-align:middle;' + (typeof invisible !== 'undefined' && invisible.includes(fields[i].id) ? "display: none;" : "") + '" colspan="'+col_f+'">' + fields[i].name + (fields[i].must ? '' : '') +  ' </td>';
 				// show field content
-				html += '<td style="'+(td_style?td_style[1]:'')+'" colspan="'+col_v+'">';
+				html += '<td style="' + (td_style?td_style[1]:'') + (typeof invisible !== 'undefined' && invisible.includes(fields[i].id) ? "display: none;" : "") + '" colspan="'+col_v+'">';
 
 				switch (fields[i].type) {
 					// FIXME: should make 'upload' not just for pics but files in general
@@ -114,7 +118,7 @@ var create_table_v3 = function (form, para) {
 							html += '<div id="'+save_id+'-show_upload_record">';
 							if (save_value.length !== 0) {
 								var files = JSON.parse(save_value);
-								for (var i in files) 
+								for (var i in files)
 									html += create_record_dev(save_id , files[i].filename, files[i].filetitle);
 							}
 							html += '</div>';
@@ -123,7 +127,7 @@ var create_table_v3 = function (form, para) {
 						} else {
 							html += '<div id="uploaded_record">';
 							html += show_record(value[fields[i].id]);
-							html += '</div>';	
+							html += '</div>';
 						}
 						break;
 					case 'upload': // 照片
@@ -143,12 +147,12 @@ var create_table_v3 = function (form, para) {
 							console.log(save_value);
 							if (save_value.length !== 0) {
 								var files = JSON.parse( save_value );
-								for (var i in files) 
+								for (var i in files)
 									html += create_img_dev(save_id, files[i].image, files[i].text);
 							}
 							html += '</div>';
 
-							html += '</form>';				
+							html += '</form>';
 						} else {
 							html += '<div id="uploaded_photo">';
 							// 舊的顯示:無文字說明
@@ -161,7 +165,7 @@ var create_table_v3 = function (form, para) {
 								for (var i in data)
 									html += show_imgs(data[i]);
 							}
-							html += '</div>';						
+							html += '</div>';
 						}
 						break;
 
@@ -171,7 +175,7 @@ var create_table_v3 = function (form, para) {
 							html += '<input type="hidden" id="'+save_id+'-encode" value="">';
 							html += '<input type="file" id="inputTxt-'+save_id+'">';
 							html += '<button id="txtBtn-'+textarea_id.length+'" onClick="uploadFile( \'1\' , \''+save_id+'\', onTxtUploaded, \''+['txt']+'\', \'inputTxt-'+save_id+'\')">上傳文字檔</button>';
-							html += '</form>';					
+							html += '</form>';
 							html += '<br>';
 							textarea_id.push(save_id);
 							html += '<textarea rows="3" cols="20" id="'+save_id+'">';
@@ -180,19 +184,19 @@ var create_table_v3 = function (form, para) {
 							html += '<textarea rows="3" cols="20" readonly="readonly">';
 							html += save_value;
 						}
-						html += '</textarea>';					
+						html += '</textarea>';
 						break;
 
 					case 'date':
 						if (write && ! fields[i].default_value  && !is_lock) {
 							html += '<input type="date" id="'+save_id+'" value="'+save_value+'" placeholder="年-月-日">';
-							date_pickers.push(save_id);	
+							date_pickers.push(save_id);
 						} else {
 							html += save_value;
 						}
 						break;
 
-					case 'autocomplete': 
+					case 'autocomplete':
 						if (write  && !is_lock) {
 							html += '<input type="text" id="' + save_id + '" value="'+save_value+'">';
 							$( function() {
@@ -215,7 +219,7 @@ var create_table_v3 = function (form, para) {
 									para.value_id = value_id;
 								SR.API.QUERY_AUTOCOMPLETE(para, function (err, result) {
 									if (err) {
-										console.log(err);	
+										console.log(err);
 									}
 									var ans = [];
 									// console.log('找到的r_form');
@@ -272,13 +276,13 @@ var create_table_v3 = function (form, para) {
 											source: ans
 										});
 									}
-								});	
-							});							
+								});
+							});
 						} else {
 							html += save_value;
-						}				
+						}
 						break;
-					case 'tag': 
+					case 'tag':
 						if (write && !is_lock) {
 							html += '<input type="text" class="tags" id="' + save_id +'" value="'+save_value+'">';
 						} else {
@@ -289,18 +293,23 @@ var create_table_v3 = function (form, para) {
 						if (write && !is_lock) {
 							// console.log('fields[i] = ')
 							// console.log(fields[i])
-							if (typeof(fields[i].option) === 'object') 
+							if (typeof(fields[i].option) === 'object')
 								var options = fields[i].option;
 							else if (typeof(fields[i].option) !== 'undefined')
 								var options = fields[i].option.split(',');
 							else
 								var options = [];
 							html += '<select id="'+ save_id + '">';
+
+							if (typeof invisible !== 'undefined' && invisible.includes(fields[i].id)) {
+								html += '<option value="">';
+							}
+
 							for (var j in options) {
 								html += '<option value="' ;
 								if (Array.isArray(options))
 									html += options[j];
-								else 
+								else
 									html += j;
 								html += '" '+(save_value!==''&&options[j]===save_value?'selected="true"':'')+'  >' + options[j] + '</option>';
 							}
@@ -309,7 +318,7 @@ var create_table_v3 = function (form, para) {
 							html += save_value;
 						}
 						break;
-					case 'multichoice': 
+					case 'multichoice':
 						let selections;
 						let showErr = err => (console.error(err))
 						if (Array.isArray(fields[i].option)) {
@@ -350,7 +359,7 @@ var create_table_v3 = function (form, para) {
 								lock_show = value_list;
 
 							// var lock_show = lock_show.split(',');
-							for (var j=0; j < value_list.length; j++) {	
+							for (var j=0; j < value_list.length; j++) {
 								html += '<option value="' + value_list[j] + '" >' + lock_show[j] + '</option>';
 							}
 							html += '</select>';
@@ -361,7 +370,7 @@ var create_table_v3 = function (form, para) {
 						break;
 					case 'password':
 						if (write && !is_lock) {
-							html += '<input type="password" id="' + save_id +'" value="'+save_value+'">';					
+							html += '<input type="password" id="' + save_id +'" value="'+save_value+'">';
 						} else {
 							html += save_value;
 						}
@@ -387,7 +396,7 @@ var create_table_v3 = function (form, para) {
 							var address = save_value.address;
 						}
 						if (write && ! fields[i].default_value  && !is_lock) {
-							// html += '<input type="text" id="' + save_id +'" value="'+save_value+'">';					
+							// html += '<input type="text" id="' + save_id +'" value="'+save_value+'">';
 							html += '<input type="text" id="'+save_id+'_postal_code" style="float: left;width: 10%;margin-left: 9px;" placeholder="郵遞區號" value="'+postal_code+'">';
 							html += '<input type="text" id="'+save_id+'_address" style="float: left;width: 85%;margin-left: 9px;" value="'+address+'">';
 						} else {
@@ -397,7 +406,7 @@ var create_table_v3 = function (form, para) {
 						}
 						break;
 					case 'radio':
-						if (write && ! fields[i].default_value && !is_lock) {	
+						if (write && ! fields[i].default_value && !is_lock) {
 							if (typeof(fields[i].option) === 'object')
 								var options = fields[i].option;
 							else if (typeof(fields[i].option) !== 'undefined')
@@ -416,7 +425,7 @@ var create_table_v3 = function (form, para) {
 						break;
 					default: // type = string or others
 						if (write && ! fields[i].default_value && !is_lock) {
-							html += '<input type="text" id="' + save_id +'" value="'+save_value+'">';					
+							html += '<input type="text" id="' + save_id +'" value="'+save_value+'">';
 						} else {
 							html += save_value;
 						}
@@ -425,7 +434,7 @@ var create_table_v3 = function (form, para) {
 
 				html += '</td>'
 			}
-			
+
 			if (br) {
 				html += '</tr>';
 			}
@@ -442,7 +451,7 @@ var create_table_v3 = function (form, para) {
 				html += '</td></tr>';
 				continue;
 			}
-			
+
 		}
 		if (show)
 			html += '<tr  ><td colspan="2"><button class="btn btn-primary" onClick="show_detail(this)">檢視細節</button></td></tr>';
@@ -455,7 +464,7 @@ var create_table_v3 = function (form, para) {
 		// 	html += '<button class="btn btn-primary" onClick="delete_field(\''+form.name+'\', \''+record_id+'\')" >刪除</button>';
 		return html;
 	}
-	
+
 	// show all valid records in the form.data.values
 	for (var record_id in form.data.values) {
 		var value = form.data.values[record_id];
@@ -463,16 +472,16 @@ var create_table_v3 = function (form, para) {
 	}
 
 	if (Object.keys(form.data.values).length === 0)
-		html += c_table(fields);	
+		html += c_table(fields);
 
-	
+
 	return html;
 }
 
 function flexform_show_table_v3(flexform_values, pa) {
 	// console.log('flexform_values = ');
 	// console.log(flexform_values);
-	
+
 	var para = pa || {};
 	// para.colStyle = [{ cssKey: cssValue }]
 	para.colStyle = para.colStyle || [];
@@ -494,7 +503,7 @@ function flexform_show_table_v3(flexform_values, pa) {
 	} else {
 		html += '<tr>';
 	}
-	// for (var i in flexform_values.field) 
+	// for (var i in flexform_values.field)
 	// 	html += '<th  onClick="javascript:flexform_sort_table(\''+flexform_table_num+'\',\''+i+'\')" >' + flexform_values.field[i].value + '</th>';
 	var count = 0;
 	var width = 1.0 / flexform_values.field.length * 100;
@@ -512,18 +521,18 @@ function flexform_show_table_v3(flexform_values, pa) {
 		}
 		flexform_values.field = new_field;
 	}
-	
+
 	function check_continue(field) { // 檢查該欄位是否要顯示
 		if (field.type === 'line' || field.type === 'print')
 			return true;
-		for (var j in hide) 
-			if ( field.id === hide[j] ) 
+		for (var j in hide)
+			if ( field.id === hide[j] )
 				return true;
 	}
 
-	
+
 	for (var i in flexform_values.field) {
-		if ( check_continue(flexform_values.field[i]) ) 
+		if ( check_continue(flexform_values.field[i]) )
 			continue;
 		var content = '';
 		content += '<li  class="drop-down-menu" onClick="javascript:switch_sort_up_down(\''+flexform_table_num+'\',\''+count+'\', this)" value="-1">';
@@ -578,8 +587,8 @@ function flexform_show_table_v3(flexform_values, pa) {
 			// html += `<td style="${obj2inlineCSS(para.colStyle[j])}">` + (typeof(flexform_values.data[i][ flexform_values.field[j].key ])==='undefined'?'':flexform_values.data[i][ flexform_values.field[j].key ]) + '</td>';
 		html += '</tr>';
 	}
-	
-	
+
+
 	html += '</table>';
 	if (para.show_lines) {
 		table_para.show_lines = para.show_lines
@@ -612,13 +621,13 @@ function check_upload_v3() {
 		for (var i in result_field.fields) {
 			var use_id = result_field.fields[i].id;
 			var o_id = result_field.fields[i].id;
-			if (typeof(add_num) !== 'undefined') 
+			if (typeof(add_num) !== 'undefined')
 				use_id += add_num;
 			// 檢查必填欄位
 
 			if (typeof(u_record_id)!== 'undefined' && u_record_id !== '')
 				use_id = u_record_id + '-' + use_id;
-			
+
 			if (result_field.fields[i].type === 'address') {
 				var address_id = use_id + '_address';
 				var use_id = use_id+ '_postal_code';
@@ -631,7 +640,7 @@ function check_upload_v3() {
 			var dom = document.getElementById(use_id);
 
 			var is_hide = false;
-			for (var t in hide) 
+			for (var t in hide)
 				if (hide[t] === result_field.fields[i].id) is_hide = true;
 			if (is_hide)
 				continue;
@@ -644,7 +653,7 @@ function check_upload_v3() {
 							err_message += result_field.fields[i].name + ' 為必填欄位\n';
 						}
 					} else {
-						
+
 						value[o_id] = $("input[name="+use_id+"]:checked").val();
 					}
 					continue;
@@ -689,7 +698,7 @@ function check_upload_v3() {
 						err_message += result_field.fields[i].name + '的郵遞區號需為數字 \n';
 					else if (dom.value.length !== 3 && dom.value.length !==0)
 						err_message += '請輸入3碼的郵遞區號 \n';
-					
+
 					value[o_id] = {postal_code: dom.value, address: dom2.value};
 				} else {
 					value[o_id] = dom.value;
@@ -701,7 +710,7 @@ function check_upload_v3() {
 					if (!f_dom)
 						f_dom = dom;
 				}
-			} 
+			}
 			if (result_field.fields[i].type === 'account' && dom.value !== '') {
 				if (!l_validateAccount(dom.value)) {
 					err_message += result_field.fields[i].name + ' 格式錯誤\n';
@@ -710,7 +719,7 @@ function check_upload_v3() {
 				}
 			}
 
-			
+
 			// 檢查上傳數量
 			if (result_field.fields[i].num) {
 				var upload_id = dom.value.split(",");
@@ -722,8 +731,8 @@ function check_upload_v3() {
 				}
 			}
 		}
-		
-		if (typeof(default_value) !== 'undefined') 
+
+		if (typeof(default_value) !== 'undefined')
 			for (var id in default_value)
 				value[id] = default_value[id];
 		values[form_name].push(value);
@@ -753,15 +762,15 @@ function check_upload_v3() {
 			}
 		}
 	}
-	
+
 	if (err_message.length !== 0) {
 		alert(err_message);
 		if (f_dom)
 			f_dom.focus();
 		return;
 	}
-	
-	if (window.flexform_v3_before_upload) 
+
+	if (window.flexform_v3_before_upload)
 		flexform_v3_before_upload(values);
 	else
 		default_upload_v3(values);
@@ -773,7 +782,7 @@ function default_upload_v3(values) {
 	var main_form = forms[use_page][0].name;
 	console.log(main_form)
 	var main_para = {form_name: main_form, values: values[main_form][0]};
-	
+
 	if (upload_record_id.length !== 0)
 		main_para.record_id = upload_record_id[0];
 	console.log('main_para')
@@ -793,7 +802,7 @@ function default_upload_v3(values) {
 		for (var i in para[use_page].related_form){
 			var form_name = para[use_page].related_form[i].form_query.name;
 			var pointer = para[use_page].related_form[i].pointer;
-			
+
 			for (var j in values[form_name]){
 				var p = {form_name: form_name, values: values[form_name][j] };
 				if (upload_record_id.length > k) {
@@ -806,7 +815,7 @@ function default_upload_v3(values) {
 				total_p.push(p);
 			}
 		}
-		
+
 		var max_num = total_p.length;
 		var run_num = 0;
 		if (max_num === 0)
@@ -854,11 +863,11 @@ function sample_create_table(array_table, table_para) {
 			html += array_table[i];
 			continue;
 		}
-		for (var j in array_table[i]) 
-			if ( !!array_table[i][j] && typeof(array_table[i][j]) === 'object' && !Array.isArray(array_table[i][j]) && !!array_table[i][j].tr ) 
+		for (var j in array_table[i])
+			if ( !!array_table[i][j] && typeof(array_table[i][j]) === 'object' && !Array.isArray(array_table[i][j]) && !!array_table[i][j].tr )
 				tr_para = array_table[i][j].tr;
 		html += '<tr ' + tr_para +'>';
-		for (var j in array_table[i]) 
+		for (var j in array_table[i])
 			if (!!array_table[i][j]) {
 				if (typeof(array_table[i][j]) === 'string')
 					html += '<td>' + array_table[i][j] + '</td>';
